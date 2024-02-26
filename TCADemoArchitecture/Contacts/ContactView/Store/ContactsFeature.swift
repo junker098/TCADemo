a019341e5ec3438528c3674144810eac4aca7c0c
Yuriy <junker098@gmail.com>
2024-02-26 17:55:37 +0200

    Implement alert logic for deleting contact, to reducer

--- TCADemoArchitecture/Contacts/ContactView/Store/ContactsFeature.swift
+++ TCADemoArchitecture/Contacts/ContactView/Store/ContactsFeature.swift
@@ -12,12 +12,19 @@ import ComposableArchitecture
 struct ContactsFeature {
     struct State: Equatable {
         @PresentationState var addContact: AddContactFeature.State?
+        @PresentationState var alert: AlertState<Action.Alert>?
         var contacts: IdentifiedArrayOf<Contact> = []
     }
     
     enum Action {
         case addButtonTapped
         case addContact(PresentationAction<AddContactFeature.Action>)
+        case alert(PresentationAction<Alert>)
+        case deleteButtonTapped(id: Contact.ID)
+        
+        enum Alert: Equatable {
+            case confirmDeletion(id: Contact.ID)
+        }
     }
     
     var body: some ReducerOf<Self> {
@@ -28,18 +35,38 @@ struct ContactsFeature {
                                                             Contact(id: UUID(), name: "")
                 )
                 return .none
-           
+                
             case let .addContact(.presented(.delegate(.saveContact(contact)))):
                 state.contacts.append(contact)
                 return .none
                 
             case .addContact:
                 return .none
+                
+            case let .deleteButtonTapped(id: id):
+                state.alert = AlertState {
+                    TextState("Are you sure?")
+                } actions: {
+                    ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+                        TextState("Delete")
+                    }
+                }
+                
+                return .none
+                
+            case let .alert(.presented(.confirmDeletion(id: id))):
+                state.contacts.remove(id: id)
+                return .none
+                
+            case .alert:
+                return .none
             }
         }
         .ifLet(\.$addContact, action: \.addContact) {
             AddContactFeature()
         }
+        
+        .ifLet(\.$alert, action: \.alert)
     }
 }
 
